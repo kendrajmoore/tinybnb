66e591bbf24eecd68284aaa6e79e189b2badfd7a
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-07-06 11:52:00 -0400

    mongoose

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/tinybnb";
+
 app.get("/", (req, res) => {
   res.send("hey");
 });
 
 app.listen(PORT, () => {
   console.log("listening.....");
+});
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("open", () => {
+  console.log("connected to mongoose");
 });
