655f433f4d7935c9087ccaa87adbb7ee95d6b02a
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-07-06 11:27:32 -0400

    fixed Port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("hey");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening.....");
 });
