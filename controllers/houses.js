fbf7560a6882359f2b966094bbc567606d3bbac4
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-07-09 12:18:21 -0400

    add  header

--- controllers/houses.js
+++ controllers/houses.js
@@ -4,45 +4,41 @@ const House = require("../models/house.js");
 
 //index;
 router.get("/house", (req, res) => {
-  res.render("index.ejs");
+  res.render("./houses/index.ejs");
 });
-// new router.get("/new", (req, res) => {
-//   res.render("new.ejs");
-// });
+router.get("/new", (req, res) => {
+  res.render("./houses/new.ejs");
+});
 
 router.post("/", (req, res) => {
   House.create(req.body, (error, house) => {
-    res.redirect("/house");
+    res.redirect("./houses/house.ejs");
   });
 });
 
 //show
 router.get("/:id", (req, res) => {
   House.findById(req.params.id, (error, house) => {
-    res.render("show.ejs", {
-      house: house
-    });
+    res.render("./houses/show.ejs");
   });
 });
 
 //Edit
 router.get("/:id/edit", (req, res) => {
   House.findById(req.params.id, (err, house) => {
-    res.render("edit.ejs", {
-      house: house
-    });
+    res.render("./houses/edit.ejs");
   });
 });
 
 router.put("/:id", (req, res) => {
   House.findByIdAndUpdate(req.params.id, req.body, (err, house) => {
-    res.redirect("/house");
+    res.redirect("./houses/house");
   });
 });
 //delete
 router.delete("/:id", (req, res) => {
   House.findByIdAndRemove(req.params.id, (err, house) => {
-    res.redirect("/house"); //redirect back to fruits index
+    res.redirect("./houses/house"); //redirect back to fruits index
   });
 });
 
