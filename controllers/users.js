932035d5cd312d143dd27ba536958c0d7c4079dd
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-07-09 16:13:39 -0400

    add sessions

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
 //root route
 router.get("/", (req, res) => {
@@ -12,25 +13,30 @@ router.get("/register", (req, res) => {
   res.render("register.ejs", { page: "register" });
 });
 
-//handle sign up logic
-router.post("/register", (req, res) => {
-  const newUser = new User({ username: req.body.username });
-  if (req.body.adminCode === process.env.ADMIN_CODE) {
-    newUser.isAdmin = true;
-  }
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      console.log(err);
-      return res.render("register.ejs", { error: err.message });
-    }
-    // passport.authenticate("local")(req, res, function() {
-    //   req.flash(
-    //     "success",
-    //     "Successfully Signed Up! Nice to meet you " + req.body.username
-    //   );
-    res.redirect("/house");
+router.post("/", (req, res) => {
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  User.create(req.body, (err, createdUser) => {
+    res.redirect("/login");
   });
 });
+//handle sign up logic
+// router.post("/register", (req, res) => {
+//   const newUser = new User({ username: req.body.username });
+//   if (req.body.adminCode === process.env.ADMIN_CODE) {
+//     newUser.isAdmin = true;
+//   }
+//   User.register(newUser, req.body.password, (err, user) => {
+//     if (err) {
+//       console.log(err);
+//       return res.render("register.ejs", { error: err.message });
+//     }
+//
+//     res.redirect("/house");
+//   });
+// });
 
 //show login form
 router.get("/login", (req, res) => {
